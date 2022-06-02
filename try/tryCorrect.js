779470e468410ffe3c5fa6e41266a17601e058fa
Ellie <zebra91528@gmail.com>
2022-06-02 17:14:56 +0800

    6/2 1.0 last version

--- try/tryCorrect.js
+++
@@ -1,9 +0,0 @@
-let accuarr = [2, 2, 3, 4, 5];
-let num = 2.5;
-let c = accuarr.length;
-console.log(c);
-for (let i = 0; i < accuarr.length; i++) {
-  if (accuarr[i] < num && num < accuarr[i + 1]) {
-    console.log(i);
-  }
-}
